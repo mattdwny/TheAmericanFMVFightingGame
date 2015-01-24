4b4147347db89743076a6492335f2f24328481f3
zofrea1 <zofrea2@gmail.com>
2015-01-23 22:47:45 -0500

    Initial commit of basic controller and input classes

---
+++ Controller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller {
+	enum Buttons {B_PUNCH, B_KICK};
+	enum Axis {A_HORIZONTAL, A_VERTICAL};
+	float[] buttons;
+	float[] axes;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ inputHolder.cs
@@ -0,0 +1,11 @@
+using System;
+namespace AssemblyCSharp
+{
+		public class inputHolder
+		{
+				public inputHolder ()
+				{
+				}
+		}
+}
+
