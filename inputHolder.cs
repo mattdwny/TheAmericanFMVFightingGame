03b9b269b250e54e7c8fc8c1739db32d9022b70b
MohammadKOmer <okm3@njit.edu>
2015-01-23 23:09:53 -0500

    I need to keep track of what actually saves, this time the namespace is gone

--- inputHolder.cs
+++ inputHolder.cs
@@ -1,18 +1,15 @@
 //john was here #Melee
-using System;
 using UnityEngine;
-
-namespace AssemblyCSharp
+using System.Collections;
+public class inputHolder
 {
-		public class inputHolder
+		public int input;
+		public float time;
+		public inputHolder (int inputInfo)
 		{
-				public int input;
-				public float time;
-				public inputHolder (int inputInfo)
-				{
-					inputInfo = input;
-					time = Time.time;
-				}
+			inputInfo = input;
+			time = Time.time;
 		}
 }
+
 
