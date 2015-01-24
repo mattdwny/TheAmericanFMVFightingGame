0c2e15c9a9be03df5e4506cba50eea389203c3d0
MohammadKOmer <okm3@njit.edu>
2015-01-23 23:56:41 -0500

    Added input cleansing

--- Controller.cs
+++ Controller.cs
@@ -3,16 +3,25 @@ using System.Collections;
 
 public class Controller {
 	enum Inputs {B_PUNCH, B_KICK,POS_A_HORIZONTAL, POS_A_VERTICAL,NEG_A_HORIZONTAL, NEG_A_VERTICAL};
-	
-	float[] buttons;
-	float[] axes;
-	// Use this for initialization
-	void Start () {
+	public LinkedList<inputHolder> inputStack;
+	public float decayTime;
+	void configure(){
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	// Update is called once per frame
+	void UpdateInput () {
+		LinkedListNode<inputHolder> CheckedInput = inputStack.first;
+		while(CheckedInput != null){
+			if(Time.time-CheckInput.time< decayTime){
+				inputHolder Temp = CheckedInput;
+				CheckedInput = CheckedInput.next;
+				inputStack.remove(Temp);
+				
+			}else{
+				CheckedInput = CheckedInput.next;
+			}
+		}
 	}
 }
