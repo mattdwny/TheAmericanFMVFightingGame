103c32843f59ffe1011fd3cf0144d56b1f42178b
MohammadKOmer <okm3@njit.edu>
2015-01-24 00:14:20 -0500

    non working demonstration code

--- Controller.cs
+++ Controller.cs
@@ -5,8 +5,12 @@ public class Controller {
 	enum Inputs {B_PUNCH, B_KICK,POS_A_HORIZONTAL, POS_A_VERTICAL,NEG_A_HORIZONTAL, NEG_A_VERTICAL};
 	public LinkedList<inputHolder> inputStack;
 	public float decayTime;
-	void configure(){
-	
+	public string p,k,h,v;
+	public Controller(string punchButton,string kickButton,string horAxis,string verAxis){
+		p=punchButton;
+		k=kickButton;
+		h=horAxis;
+		v=verAxis;
 	}
 	
 	
@@ -22,6 +26,15 @@ public class Controller {
 			}else{
 				CheckedInput = CheckedInput.next;
 			}
+		}
+		if(input.getKeyDown(p)){
+			inputStack.AddLast(new inputHoled(B_punch));
+		}if(input.getKeyDown(k)){
+			inputStack.AddLast(new inputHoled(b_Kick));
+		}if(input.getKeyDown(p)){
+			inputStack.AddLast(new inputHoled(B_punch));
+		}if(input.getKeyDown(p)){
+			inputStack.AddLast(new inputHoled(B_punch));
 		}
 	}
 }
