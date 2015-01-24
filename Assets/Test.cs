b079ff1555b46fd813eec898aa86ea2d7fab4c28
Ed Conroy <epc5@njit.edu>
2015-01-24 06:02:52 -0500

    Create Test.cs

---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+	public MovieTexture mov;
+	// Use this for initialization
+	void Start () {
+		mov.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
