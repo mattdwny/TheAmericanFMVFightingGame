76c01075fd1a49fd86eaa77d5fc0e0bf889ec6b1
MohammadKOmer <okm3@njit.edu>
2015-01-23 23:12:00 -0500

    Changed up the input enum

--- Controller.cs
+++ Controller.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class Controller {
-	enum Buttons {B_PUNCH, B_KICK};
-	enum Axis {A_HORIZONTAL, A_VERTICAL};
+	enum Inputs {B_PUNCH, B_KICK,POS_A_HORIZONTAL, POS_A_VERTICAL,NEG_A_HORIZONTAL, NEG_A_VERTICAL};
+	
 	float[] buttons;
 	float[] axes;
 	// Use this for initialization
