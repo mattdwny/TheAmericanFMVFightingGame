e3c3513bc0a6ad3a338312b83ffb57313fad8998
Ed Conroy <epc5@njit.edu>
2015-01-24 06:03:15 -0500

    Create Fighter.cs

---
+++ Assets/Fighter.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using System.Collections;
+
+public class Fighter : MonoBehaviour {
+	BoxCollider2D hitbox;
+	public GameObject enemy;
+	public enum FighterState {PUNCH, KICK, JUMP, BLOCK, AIRPUNCH, AIRKICK, NULL};
+	public FighterState state = FighterState.NULL;
+	public State[] states;
+
+	public MovieTexture vid;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		bool redo = false;
+
+		REDO:
+/*
+		switch(state)
+		{
+			case FighterState.NULL:
+				if (Buttons.jump + x < Time.time)
+				{
+					this.state = FighterState.JUMP;
+					redo = true;
+				}
+				else if (Buttons.punch + x < Time.time) 
+				{
+					this.state = FighterState.PUNCH;
+					redo = true;
+				}
+				else if (Buttons.kick + x < Time.time) 
+				{
+					this.state = FighterState.KICK;
+					redo = true;
+				}
+				else if (Buttons.airPunch + x < Time.time) 
+				{
+					this.state = FighterState.AIRPUNCH;
+					redo = true;
+				}
+				else if (Buttons.airKick + x < Time.time) 
+				{
+					this.state = FighterState.AIRKICK;
+					redo = true;
+				}
+				else if (Buttons.block + x < Time.time) 
+				{
+					this.state = FighterState.BLOCK;
+					redo = true;
+				}
+				break;
+			case FighterState.PUNCH:
+				
+				//Punch();
+				break;
+			case FighterState.KICK:
+
+				//Kick();
+				break;
+			case FighterState.JUMP:
+
+				//Jump();
+			/*
+				transform.position.y += vy;
+				--vy;
+				if(transform.position.y <= floor && vy != 15)
+				{
+					transform.position.y = floor;
+					vy = 15;
+					state = FighterState.NULL;
+				}
+				break;
+			case FighterState.AIRPUNCH:
+
+				//AirPunch();
+				break;
+			case FighterState.AIRKICK:
+
+				//AirKick();
+				break;
+			case FighterState.BLOCK:
+
+				//Block();
+				break;
+			default:
+				Debug.Log ("You Dun Fucked Up");
+				break;
+		}
+		*/
+
+		if(redo) goto REDO;
+	}
+}
+
+
